b15f5314939a9f889b5429e69a9952cdbe641fd1
Mario Davila <mdasmat@gmail.com>
2026-05-07 15:50:08 -0500

    Avance 8

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,49 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.time.Duration;
-
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium. *;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.time.Duration;
 
 
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+        public class Main {
+            public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
+                WebDriver driver = new ChromeDriver();
 
-        // configurar espera explicita
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+                // configurar espera explicita
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-        driver.get("https://www.facebook.com");
+                Actions actions = new Actions(driver);
 
-        Actions actions = new Actions(driver);
+                driver.get("https://www.facebook.com");
 
-        WebElement email = driver.findElement(By.id("_R_64mkqsr9pb6amH1_"));
+                WebElement loginButtom = wait.until(ExpectedConditions.elementToBeClickable((By.xpath("//div[@role='button' and @aria-label='Iniciar sesión']"))));
 
-        actions.contextClick(email).perform();
-    }
-}
+                loginButtom.click();
+
+                WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(10));
+                WebElement errorMessage = wait1.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[href*='login/identify']")));
+
+                //validar la visibilidad
+                System.out.println("Mensaje de error is visible: " + errorMessage.isDisplayed());
+
+                //validar color del mensaje
+                String color = errorMessage.getCssValue("color");
+                System.out.println("Color del mensaje de error: " + color);
+
+                // validar el tamaño del mensaje
+                Dimension size = errorMessage.getSize();
+                System.out.println("Ancho del mensaje de error" + size.getWidth());
+
+
+            }
+        }
